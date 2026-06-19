22e070828403fd9797dad226e6eb16ecffe885d8
Antonio2Pedro <antoniokianguebeniak@gmail.com>
2026-06-19 16:37:17 +0100

    Initialize Supabase client in supabase.js

---
+++ cria-um-site-de-venda-online/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://sb_publishable_Vcu9RsgsRYGadIYzzMUObA_9SPVpCAx.supabase.co";
+const SUPABASE_KEY = "sb_secret_PveD-AJs5dVtBTjFcfAxNQ_B_ta9nK2";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
