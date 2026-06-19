7165ecb44bf354f690ea0ea20eae4a57a8766124
Antonio2Pedro <antoniokianguebeniak@gmail.com>
2026-06-19 16:43:14 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://sb_publishable_Vcu9RsgsRYGadIYzzMUObA_9SPVpCAx.supabase.co";
-const SUPABASE_KEY = "sb_secret_PveD-AJs5dVtBTjFcfAxNQ_B_ta9nK2";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxZmVkcWZ0eGhodXR5eW1oemVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4NzQ4NzAsImV4cCI6MjA5NzQ1MDg3MH0.76ZL2wKiuAMIXMXYvKoUrGT2yPnvoNM2Ebg94icwgUM";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
